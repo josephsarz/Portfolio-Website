1bcc822f448f3ce93a22e6e9aa6da69f29132917
femiaghedo <aghedojoe007@gmail.com>
2020-05-31 05:14:08 +0100

    added typist for text movement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Typist from 'react-typist';
 
 function App() {
   return (
     <body>
-    <div>
 
-      {/* <div class="typewriter-text">I am a boss man.</div> */}
-     
+  
+    <div id="container">
+
+    <div className="typewriter">
+      <Typist>
+        Hi, My name is <span className="highlight">Femi</span>.
+        <Typist.Backspace count={20} delay={1000} />
+        <Typist.Delay ms={750} />
+        I love to <Typist.Delay ms={500} /><span className="highlight">eat</span>.
+        <Typist.Backspace count={4} delay={1000} />
+        <Typist.Delay ms={750} /><span className="highlight">watch anime</span>.
+        <Typist.Backspace count={12} delay={1000} />
+        <Typist.Delay ms={750} /><span className="highlight">code</span>.
+      </Typist>
+    </div>
+     <div className="details">
       <h1 className="name-placeholder"> Aghedo Joseph Femi </h1>
 
         <p className="description-placeholder">
@@ -18,9 +31,12 @@ function App() {
           <br></br>
           Software Engineer
         </p> 
+
+        </div>
     </div>
     </body>
   );
 }
+
 
 export default App;
